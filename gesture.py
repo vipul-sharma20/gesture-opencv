50abeea0de445a0805f2192eab2f4534becfa4fc
vipul <vipul.sharma20@gmail.com>
2014-12-26 01:13:18 +0530

    added gesture recognitino based on convexity defects

--- gesture.py
+++ gesture.py
@@ -11,6 +11,7 @@ while(cap.isOpened()):
     blurred = cv2.GaussianBlur(grey, value, 0)
     _, thresh1 = cv2.threshold(blurred, 127, 255,
                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    cv2.imshow('Thresholded', thresh1)
     contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
             cv2.CHAIN_APPROX_NONE)
     max_area = -1
@@ -27,7 +28,6 @@ while(cap.isOpened()):
     drawing = np.zeros(crop_img.shape,np.uint8)
     cv2.drawContours(drawing,[cnt],0,(0,255,0),0)
     cv2.drawContours(drawing,[hull],0,(0,0,255),0)
-    #cv2.imshow('drw', drawing)
     hull = cv2.convexHull(cnt,returnPoints = False)
     defects = cv2.convexityDefects(cnt,hull)
     count_defects = 0
@@ -43,26 +43,28 @@ while(cap.isOpened()):
         angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
         if angle <= 90:
             count_defects += 1
-            cv2.circle(crop_img,far,5,[0,0,255],-1)
+            cv2.circle(crop_img,far,1,[0,0,255],-1)
         #dist = cv2.pointPolygonTest(cnt,far,True)
         cv2.line(crop_img,start,end,[0,255,0],2)
         #cv2.circle(crop_img,far,5,[0,0,255],-1)
-        print start
     if count_defects == 1:
         cv2.putText(img,"I am Vipul", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     elif count_defects == 2:
-        cv2.putText(img,"This is cool", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        str = "This is a basic hand gesture recognizer"
+        print str
+        cv2.putText(img, str, (5,50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
     elif count_defects == 3:
         cv2.putText(img,"This is 4 :P", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     elif count_defects == 4:
         cv2.putText(img,"Hi!!!", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     else:
-        cv2.putText(img,"Hello World!!!", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    cv2.imshow('drawing', drawing)
-    #cv2.imshow('thresholded', thresh1)
-    cv2.imshow('end', crop_img)
-    cv2.imshow('original', img)
-    cv2.imshow('Thresholded', thresh1)
+        cv2.putText(img,"Hello World!!!", (50,50),\
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+    #cv2.imshow('drawing', drawing)
+    #cv2.imshow('end', crop_img)
+    cv2.imshow('Gesture', img)
+    all_img = np.hstack((drawing, crop_img))
+    cv2.imshow('Contours', all_img)
     k = cv2.waitKey(10)
     if k == 27:
         break
