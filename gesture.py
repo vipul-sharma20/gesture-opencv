0be2eeb5062c9f6cfb32a57a16bb4adb70ec3a6c
Achilles <achilles@achillesrasquinh@gmail.com>
2016-09-20 11:10:00 +0530

    one liner lambda alternative for finding max contour area

--- gesture.py
+++ gesture.py
@@ -22,14 +22,8 @@ while(cap.isOpened()):
         contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
                cv2.CHAIN_APPROX_NONE)
 
-    max_area = -1
-    for i in range(len(contours)):
-        cnt=contours[i]
-        area = cv2.contourArea(cnt)
-        if(area>max_area):
-            max_area=area
-            ci=i
-    cnt=contours[ci]
+    cnt = max(contours, key = lambda x: cv2.contourArea(x))
+    
     x,y,w,h = cv2.boundingRect(cnt)
     cv2.rectangle(crop_img,(x,y),(x+w,y+h),(0,0,255),0)
     hull = cv2.convexHull(cnt)
