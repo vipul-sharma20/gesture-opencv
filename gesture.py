d64b19779870cdd7c7e671fa648793cd97a3d2e2
Vipul Sharma <vipul.sharma20@gmail.com>
2017-06-13 17:58:24 +0530

    refactor code

--- gesture.py
+++ gesture.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
 import math
+
+
 cap = cv2.VideoCapture(0)
 while(cap.isOpened()):
     # read image
     ret, img = cap.read()
 
     # get hand data from the rectangle sub window on the screen
-    cv2.rectangle(img,(300,300),(100,100),(0,255,0),0)
+    cv2.rectangle(img, (300,300), (100,100), (0,255,0),0)
     crop_img = img[100:300, 100:300]
 
     # convert to grayscale
@@ -27,10 +29,10 @@ while(cap.isOpened()):
     # check OpenCV version to avoid unpacking error
     (version, _, _) = cv2.__version__.split('.')
 
-    if version is '3':
+    if version == '3':
         image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
                cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    elif version is '2':
+    elif version == '2':
         contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
                cv2.CHAIN_APPROX_NONE)
 
@@ -38,24 +40,24 @@ while(cap.isOpened()):
     cnt = max(contours, key = lambda x: cv2.contourArea(x))
 
     # create bounding rectangle around the contour (can skip below two lines)
-    x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(crop_img,(x,y),(x+w,y+h),(0,0,255),0)
+    x, y, w, h = cv2.boundingRect(cnt)
+    cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
 
     # finding convex hull
     hull = cv2.convexHull(cnt)
 
     # drawing contours
     drawing = np.zeros(crop_img.shape,np.uint8)
-    cv2.drawContours(drawing,[cnt],0,(0,255,0),0)
-    cv2.drawContours(drawing,[hull],0,(0,0,255),0)
+    cv2.drawContours(drawing,[cnt], 0, (0, 255, 0), 0)
+    cv2.drawContours(drawing,[hull], 0,(0, 0, 255), 0)
 
     # finding convex hull
-    hull = cv2.convexHull(cnt,returnPoints = False)
+    hull = cv2.convexHull(cnt, returnPoints=False)
 
     # finding convexity defects
-    defects = cv2.convexityDefects(cnt,hull)
+    defects = cv2.convexityDefects(cnt, hull)
     count_defects = 0
-    cv2.drawContours(thresh1, contours, -1, (0,255,0), 3)
+    cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
 
     # applying Cosine Rule to find angle for all defects (between fingers)
     # with angle > 90º and ignore defects
@@ -77,26 +79,26 @@ while(cap.isOpened()):
         # ignore angles > 90º and highlight rest with red dots
         if angle <= 90:
             count_defects += 1
-            cv2.circle(crop_img,far,1,[0,0,255],-1)
+            cv2.circle(crop_img, far, 1, [0,0,255], -1)
         #dist = cv2.pointPolygonTest(cnt,far,True)
 
         # draw a line from start to end i.e. the convex points (finger tips)
         # (can skip this part)
-        cv2.line(crop_img,start,end,[0,255,0],2)
+        cv2.line(crop_img,start, end, [0,255,0], 2)
         #cv2.circle(crop_img,far,5,[0,0,255],-1)
 
     # define actions required
     if count_defects == 1:
-        cv2.putText(img,"I am Vipul", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        cv2.putText(img,"I am Vipul", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     elif count_defects == 2:
         str = "This is a basic hand gesture recognizer"
-        cv2.putText(img, str, (5,50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+        cv2.putText(img, str, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
     elif count_defects == 3:
-        cv2.putText(img,"This is 4 :P", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        cv2.putText(img,"This is 4 :P", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     elif count_defects == 4:
-        cv2.putText(img,"Hi!!!", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        cv2.putText(img,"Hi!!!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     else:
-        cv2.putText(img,"Hello World!!!", (50,50),\
+        cv2.putText(img,"Hello World!!!", (50, 50),\
                     cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     #cv2.imshow('drawing', drawing)
     #cv2.imshow('end', crop_img)
